6e6d8d0bf1f959d50d886c379a5eea253646f708
Bade Boy I. Tulabing <badeboytulabing0127@gmail.com>
2024-04-16 13:10:20 +0800

    notions

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<?php require_once("parts/head.php"); ?>
+<body>
+
+
+<?php require_once("parts/js.php"); ?>
+</body>
+</html>
---
+++ parts/footer.php
@@ -0,0 +1,13 @@
+<footer class="text-center text-white fixed-bottom" style="background-color: #9FA4A1;">
+  <!-- Grid container -->
+  <!-- <div class="container p-4"></div> -->
+  <div class="container"></div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © <?php echo date("Y"); ?> Copyright:
+    <a class="text-white text-decoration-none animated flash slow infinite" href="#">Footer</a>
+  </div>
+  <!-- Copyright -->
+</footer>
